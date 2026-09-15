a757ac7c5b56f429a91a3e00e58cb0e7f98ec428
bhavana@github@123 <bhavanareddy.ngb@gmail.com>
2026-09-15 13:04:40 +0530

    Delete api implemented

--- app.js
+++ app.js
@@ -22,22 +22,47 @@ app.use(bodyParser.json());
 //   res.send("Welcome the node js world");
 // });
 
-app.get("/", (req, res) => {
+// app.get("/product", (req, res) => {
+//   console.log("Got a GET request",req);
+//   res.json({
+//     data: {
+//       status: 200,
+//       message: "Welcome to the node js world",
+//     },
+//   });
+// });
+
+app.get("/product", (req, res) => {
+  console.log("Got a GET request");
+  console.log("Product ID:", req.query.productId);
+
   res.json({
     data: {
       status: 200,
       message: "Welcome to the node js world",
-    },
+      productId: req.query.productId
+    }
   });
 });
 
-app.post('/', (req, res) => {
+app.post('/product', (req, res) => {
   console.log('Got a POST request');
   console.log('Request body:', req.body);
   res.json({
     data: {
       status: 200,
       message: {data: req.body},
+    },
+  });
+});
+
+app.delete('/product/:id', (req, res) => {
+  console.log('Got a DELETE request');
+  console.log('Request params:', req.params);
+  res.json({
+    data: {
+      status: 200,
+      message: "Delete the user with id " + req.params.id,
     },
   });
 });
